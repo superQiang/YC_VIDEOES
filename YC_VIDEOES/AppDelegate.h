91f97efe35fae201a3c3bcbd0d45eff5a51a3188
superQiang <donghongqiangdhq@163.com>
2013-03-21 15:25:30 +0800

    Initial Commit

---
+++ YC_VIDEOES/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YC_VIDEOES
+//
+//  Created by apple on 13-3-21.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ YC_VIDEOES/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  YC_VIDEOES
+//
+//  Created by apple on 13-3-21.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
